2d942211dafd35ab0d2ceb65ff7284c2e57b5a1f
AsiaWu_a <wuasia2007@hotmail.com>
2026-09-20 21:30:52 +0800

    java 9/20

--- java_homework/SurvivalTime.java
+++ java_homework/SurvivalTime.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 public class SurvivalTime {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int year = sc.nextINt();
+        System.out.print("输入年份:");
+        int year = sc.nextInt();
+        System.out.print("输入月份:");
+        int mon = sc.nextInt();
+        System.out.print("输入天数:");
+        int day = sc.nextInt();
+        LocalDate today = LocalDate.now();
+        System.out.print("今日日期:" + today);
     }
 }
